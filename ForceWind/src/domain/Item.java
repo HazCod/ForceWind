ae92b110a3a3f547729ead5102c9847dc3bbe337
Nindustries <Nindustries@192.168.0.149>
2013-08-28 13:11:16 +0200

    Changed to TreeMap, corrected Tile loading, tiles are ordened, Tiles are no longer owner of coordinates.

--- ForceWind/src/domain/Item.java
+++ ForceWind/src/domain/Item.java
@@ -46,31 +46,4 @@ public class Item extends GameObject {
         this.effect = effect;
     }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the quantity
-     */
-    public int getQuantity() {
-        return quantity;
-    }
-
-    /**
-     * @param quantity the quantity to set
-     */
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 }
